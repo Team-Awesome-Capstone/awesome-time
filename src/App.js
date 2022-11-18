7888090843da2cc146d1c7701ef0085e63e06606
Kadir0-7 <101948177+Kadir0-7@users.noreply.github.com>
2022-11-17 19:30:08 -0500

    map api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import Navbar from './components/navBar';
 import './App.css';
-import React from 'react';
+import React, {useMemo} from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 function App() {
+
+   
+
+ const { isLoaded } = useLoadScript({
+   googleMapsApiKey: 'AIzaSyBkdHcWFWGDabP2RdWZFeBaeJssamRfM9s',
+ });
+
+ if (!isLoaded) return <div>Loading...</div>;
+  
+
+
+
   return (
     <div className='App'>
-      <React.Fragment>
-        <Navbar/>
-      </React.Fragment>
+      <Navbar />
+      <Map />
     </div>
   );
 }
+function Map() {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
 
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName='map-container'>
+      <Marker position={center} />
+    </GoogleMap>
+  );
+}
 export default App;
