85313e43db5a2456cfa71f0c097c0795e37270c5
Kadir0-7 <101948177+Kadir0-7@users.noreply.github.com>
2022-11-18 21:23:47 -0500

    map Api complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import Navbar from './components/navBar';
 import './App.css';
-import React, {useMemo} from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
 
+  
+const libraries = ['places'];
+const mapContainerStyle = {
+  height: '100vh',
+  width: '100vw',
+};
+const options = {
+  
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 43.6532,
+  lng: -79.3832,
+};
+  
 function App() {
 
-   
+const { isLoaded, loadError } = useLoadScript({
+  googleMapsApiKey: 'AIzaSyBkdHcWFWGDabP2RdWZFeBaeJssamRfM9s',
+  libraries,
+});
+const mapRef = React.useRef();
+const onMapLoad = React.useCallback((map) => {
+  mapRef.current = map;
+}, []);
 
- const { isLoaded } = useLoadScript({
-   googleMapsApiKey: 'AIzaSyBkdHcWFWGDabP2RdWZFeBaeJssamRfM9s',
- });
+const panTo = React.useCallback(({ lat, lng }) => {
+  mapRef.current.panTo({ lat, lng });
+  mapRef.current.setZoom(14);
+}, []);
 
- if (!isLoaded) return <div>Loading...</div>;
+if (loadError) return 'Error';
+if (!isLoaded) return 'Loading...';
+
+
   
 
 
@@ -19,17 +58,98 @@ function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Map />
+      <div className='bod'>
+      <Locate panTo={panTo} />
+      <Search panTo={panTo} />
+      </div>
+     <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerStyle={mapContainerStyle}
+        options={options}
+        mapContainerClassName='map-container'
+        onLoad={onMapLoad}
+      >
+        <Marker position={center} />
+      </GoogleMap> 
+      
+      
     </div>
   );
 }
-function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 43.6532, lng: () => -79.3832 },
+      radius: 100 * 1000,
+    },
+  });
+
+  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log(' Error: ', error);
+    }
+  };
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName='map-container'>
-      <Marker position={center} />
-    </GoogleMap>
+    <div className='search'>
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder='Search your location'
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === 'OK' &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+function Locate({ panTo }) {
+  return (
+    <button
+      className='locate'
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+     Click For Curr-Location
+    </button>
   );
 }
 export default App;
